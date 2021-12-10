fe593ddae18c7371b11459aa0eadf2a206a1e456
gangsararyop <86463063+gangsararyop@users.noreply.github.com>
2021-12-11 03:01:07 +0700

    WJ-11 bisa input data

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -21,4 +21,32 @@ module.exports = {
       return res.status(500).send({ message: error.message });
     }
   },
+
+  getPersonalData: async (req, res) => {
+    const connDb = connection.promise();
+
+    try {
+      let sql = `select first_name, last_name, email, gender, date_of_birth from user where id = ?`;
+      let [personalData] = await connDb.query(sql, [req.params.userId]);
+
+      return res.status(200).send(personalData);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: error.message });
+    }
+  },
+
+  inputPersonalData: async (req, res) => {
+    const connDb = connection.promise();
+
+    try {
+      let sql = `update user set ? where id = ? and role_id = 3`;
+      await connDb.query(sql, [req.body, req.params.userId]);
+
+      return res.status(200).send({ message: "Data berhasil disimpan" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({ message: error.message });
+    }
+  },
 };
--- src/routes/ProfileRoute.js
+++ src/routes/ProfileRoute.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const uploader = require("./../helpers/Uploader");
 const { profileController } = require("./../controllers");
-const { uploadProfilePicture } = profileController;
+const { inputPersonalData } = require("../controllers/profileController");
+const { uploadProfilePicture, getPersonalData } = profileController;
 
 const uploadFile = uploader("/photo-profile", "PROF").fields([
   { name: "image", maxCount: 3 },
 ]);
 
 router.patch("/upload-photo/:userId", uploadFile, uploadProfilePicture);
+router.get("/personal-data/:userId", getPersonalData);
+router.post("/edit/personal-data/:userId", inputPersonalData);
 
 module.exports = router;
