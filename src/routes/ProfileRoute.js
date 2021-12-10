f3b4383aa69cb28b7b3a891314aeef707c7fbad8
gangsararyop <86463063+gangsararyop@users.noreply.github.com>
2021-12-11 00:14:52 +0700

    WJ-13 upload berhasil

--- src/routes/ProfileRoute.js
+++ src/routes/ProfileRoute.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const uploader = require("./../helpers/Uploader");
 const { profileController } = require("./../controllers");
-const { uploadProfilePicture, categories } = profileController;
+const { uploadProfilePicture } = profileController;
 
 const uploadFile = uploader("/photo-profile", "PROF").fields([
   { name: "image", maxCount: 3 },
 ]);
 
 router.patch("/upload-photo/:userId", uploadFile, uploadProfilePicture);
-router.get("/category", categories);
 
 module.exports = router;
