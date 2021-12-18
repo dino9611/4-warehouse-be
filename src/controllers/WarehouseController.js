019ddc4da98a6ea922d345d34382f588235efc77
maulanas13 <maulana.septanto@hotmail.com>
2021-12-18 13:07:45 +0700

    [WJ-44] Created add warehouse route & controller

--- src/controllers/WarehouseController.js
+++ src/controllers/WarehouseController.js
@@ -18,4 +18,34 @@ module.exports = {
             return res.status(500).send({ message: error.message || "Server error" });
         };
     },
+    addWarehouse: async (req, res) => {
+        console.log("Jalan /warehouse/add");
+        const conn = await connection.promise().getConnection();
+
+        // Destructure data input produk dari FE, utk insert ke MySql
+        const {
+          warehouse_name,
+          warehouse_address
+        } = req.body;
+
+        try {
+          await conn.beginTransaction(); // Aktivasi table tidak permanen agar bisa rollback/commit permanent
+
+          let sql = `INSERT INTO warehouse SET ?`;
+          let addDataWh = {
+            name: warehouse_name,
+            address: warehouse_address
+          };
+          const [addResult] = await conn.query(sql, addDataWh);
+
+          await conn.commit(); // Commit permanent data diupload ke MySql klo berhasil
+          conn.release();
+          return res.status(200).send({ message: "Berhasil tambah warehouse" });
+        } catch (error) {
+          await conn.rollback(); // Rollback data klo terjadi error/gagal
+          conn.release();
+          console.log(error);
+          return res.status(500).send({ message: error.message || "Server error" });
+        };
+    }
 }
