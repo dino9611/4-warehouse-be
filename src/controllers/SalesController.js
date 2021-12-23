ce71c0952894225a942bc213b6fb7afc870a1364
maulanas13 <maulana.septanto@hotmail.com>
2021-12-23 16:33:38 +0700

    [WJ-25] Add various get sales data route & controller

--- src/controllers/SalesController.js
+++ src/controllers/SalesController.js
@@ -75,6 +75,31 @@ module.exports = {
             return res.status(500).send({ message: error.message || "Server error" });
         }
     },
+    getYearlyRevenue: async (req, res) => {
+        console.log("Jalan /sales/yearly-revenue");
+        const conn = await connection.promise().getConnection();
+        const {filter_year} = req.headers;
+
+        try {
+            let sql = `
+                SELECT SUM(revenue) AS total_yearly
+                FROM (SELECT DATE_FORMAT(o.create_on, "%b") AS month, SUM(od.price) AS revenue
+                    FROM order_detail AS od
+                    JOIN orders o
+                    ON od.orders_id = o.id
+                    WHERE o.create_on <= NOW() AND o.create_on >= DATE_ADD(NOW(), interval - 12 MONTH) AND status_id = ? AND YEAR(o.create_on) = ?
+                    GROUP BY DATE_FORMAT(o.create_on, "%m-%Y")) AS sub;
+            `
+            const [revenueResult] = await conn.query(sql, [2, parseInt(filter_year)]);
+
+            conn.release();
+            return res.status(200).send(revenueResult[0]);
+        } catch (error) {
+            conn.release();
+            console.log(error);
+            return res.status(500).send({ message: error.message || "Server error" });
+        }
+    },
     getStatusContribution: async (req, res) => {
         console.log("Jalan /sales/status-contribution");
         const conn = await connection.promise().getConnection();
@@ -159,6 +184,28 @@ module.exports = {
             return res.status(500).send({ message: error.message || "Server error" });
         }
     },
+    getTotalProdSold: async (req, res) => {
+        console.log("Jalan /sales/prod-sold");
+        const conn = await connection.promise().getConnection();
+        const {filter_year} = req.headers;
+
+        try {
+            let sql = `
+                SELECT SUM(qty) AS total_qty_sold FROM order_detail AS od
+                JOIN orders o
+                ON od.orders_id = o.id
+                WHERE status_id = ? AND YEAR(o.create_on) = ?;
+            `
+            const [usersResult] = await conn.query(sql, [2, parseInt(filter_year)]);
+
+            conn.release();
+            return res.status(200).send(usersResult[0]);
+        } catch (error) {
+            conn.release();
+            console.log(error);
+            return res.status(500).send({ message: error.message || "Server error" });
+        }
+    },
     getTopUsers: async (req, res) => {
         console.log("Jalan /sales/top-users");
         const conn = await connection.promise().getConnection();
@@ -180,6 +227,60 @@ module.exports = {
 
             conn.release();
             return res.status(200).send(topProdResult);
+        } catch (error) {
+            conn.release();
+            console.log(error);
+            return res.status(500).send({ message: error.message || "Server error" });
+        }
+    },
+    getTotalUsers: async (req, res) => {
+        console.log("Jalan /sales/total-users");
+        const conn = await connection.promise().getConnection();
+
+        try {
+            let sql = `
+                SELECT COUNT(id) AS total_users FROM user
+                WHERE role_id = ?;
+            `
+            const [usersResult] = await conn.query(sql, 3);
+
+            conn.release();
+            return res.status(200).send(usersResult[0]);
+        } catch (error) {
+            conn.release();
+            console.log(error);
+            return res.status(500).send({ message: error.message || "Server error" });
+        }
+    },
+    getCategoryContribution: async (req, res) => {
+        console.log("Jalan /sales/category-contribution");
+        const conn = await connection.promise().getConnection();
+        const {filter_year} = req.headers;
+
+        try {
+            let sql = `
+                SELECT 
+                c.category, 
+                SUM(od.price) AS amount, 
+                ROUND(SUM(od.price) * 100 / (SELECT SUM(price) AS tc FROM order_detail 
+                    JOIN orders o 
+                    ON order_detail.orders_id = o.id 
+                    WHERE o.status_id = ? AND YEAR(o.create_on) = ?), 1) AS contribution 
+                    FROM category AS c
+                        JOIN product p
+                        ON c.id = p.category_id
+                        JOIN order_detail od
+                        ON p.id = od.product_id
+                        JOIN orders o
+                        ON od.orders_id = o.id 
+                        WHERE o.status_id = ? AND YEAR(o.create_on) = ? 
+                        GROUP BY c.category
+                        ORDER BY amount DESC;
+            `
+            const [categoryResult] = await conn.query(sql, [2, parseInt(filter_year), 2, parseInt(filter_year)]);
+
+            conn.release();
+            return res.status(200).send(categoryResult);
         } catch (error) {
             conn.release();
             console.log(error);
--- src/routes/SalesRoute.js
+++ src/routes/SalesRoute.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const { salesController } = require("./../controllers");
-const { getMonthlyRevenue, getPotentialRevenue, getStatusContribution, getTopProdQty, getTopProdVal, getTopUsers } = salesController;
+const { 
+    getMonthlyRevenue, 
+    getPotentialRevenue, 
+    getYearlyRevenue,
+    getStatusContribution, 
+    getTopProdQty, 
+    getTopProdVal, 
+    getTotalProdSold,
+    getTopUsers, 
+    getTotalUsers, 
+    getCategoryContribution,
+} = salesController;
 
 router.get("/monthly-revenue", getMonthlyRevenue);
 router.get("/potential-revenue", getPotentialRevenue);
+router.get("/yearly-revenue", getYearlyRevenue);
 router.get("/status-contribution", getStatusContribution);
 router.get("/top-prod-qty", getTopProdQty);
 router.get("/top-prod-val", getTopProdVal);
+router.get("/prod-sold", getTotalProdSold);
 router.get("/top-users", getTopUsers);
+router.get("/total-users", getTotalUsers);
+router.get("/category-contribution", getCategoryContribution);
 
 module.exports = router;
